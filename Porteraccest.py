a559810e3615cf5d73606d1274db43f971d51be0
Spencer Krieger <spencer.krieger@gmail.com>
2020-07-02 11:15:09 -0700

    add updated Porter accuracy estimator

--- Porteraccest.py
+++ Porteraccest.py
@@ -3,24 +3,46 @@ import sys
 def function(x,a,b):
     return a * x + b
 
-def porteracctest(popt,predfile,confstartloc,q3startloc):
+def getconf(lines,startloc,returnarray=False):
+    conf = 0
+    count = 0
+    outintline = []
+    for i in range(startloc,len(lines),7):
+        line = lines[i].strip()
+        intline = [int(l) for l in line.strip()]
+        outintline += intline
+        conf += sum(intline)
+        count += len(line)
+    if returnarray == False:
+        return conf / count
+    else:
+        return conf/count,outintline
+
+def porteracctest(predfile,confstartloc,q3startloc,a,b):
     predf = open(predfile,'r').readlines()
     currline = 0
     diffs = []
     estaccs = []
     predseqs = []
     q3s = []
-    for i in range(1,len(truef),2):
-        prevline = currline
+    prevline = currline
+    currline += 1
+    while 'Query_name' not in predf[currline] and currline < len(predf) - 1 and 'Query served' not in predf[currline]:
         currline += 1
-        while 'Query_name' not in predf[currline] and currline < len(predf) - 1 and 'Query served' not in predf[currline]:
-            currline += 1
-        conf = getconf(predf[prevline:currline],confstartloc)
-        a = 0.06717725 
-        b = 0.3950873643451706
-        estacc = function(conf,a,b)
-        estaccs.append(estacc)
-    estpred = [(estaccs[i],predseqs[i],q3s[i]) for i in range(len(estaccs))]
-    #estpred = [(estaccs[i],predseqs[i]) for i in range(len(estaccs))]
-    return estpred
+    conf = getconf(predf[prevline:currline],confstartloc)
+    estacc = function(conf,a,b)
+    return estacc
 
+predfile = sys.argv[1]
+if sys.argv[2] == '8state':
+    confstartloc = 7
+    q3startloc = 6
+    a = 0.06717725 
+    b = 0.3950873643451706
+elif sys.argv[2] == '3state':
+    confstartloc = 5
+    q3startloc = 4
+    a = 0.05381087 
+    b = 0.5139789336088341
+
+print('estimated accuracy: {:.03f}'.format(porteracctest(predfile,confstartloc,q3startloc,a,b)))
